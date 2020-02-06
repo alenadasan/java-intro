301d4e56931e2ee5eca46c114034c12da9ac6103
ale <ale.nadasan@gmail.com>
2020-02-06 12:52:48 +0200

    - some edits on StringDemo class - added a demo html file

--- src/test/java/StringDemo.java
+++ src/test/java/StringDemo.java
@@ -13,41 +13,35 @@ public class StringDemo {
         String replacedCharsString = s.replace('M', 'P');
         String substring = s.substring(0, 2);
 
-        boolean startsWith = s.startsWith("T");
+        String prefix = "This is a";
+        boolean startsWith = s.startsWith(prefix);
         boolean isEmpty = s.isEmpty();
         boolean containsString = s.contains("DUMMY");
 
-        int indexOf = s.indexOf("DUMMY");
         int match = s.compareToIgnoreCase("THIS is a dummy string    ");
+        int indexOf = s.indexOf("s");
         int length = s.length();
 
         char firstChar = s.charAt(0);
         char lastChar = s.charAt(length - 1);
 
 
-        System.out.println("Trimmed string: " + trimmedString);
+        System.out.println("Trimmed string: " + trimmedString + "*");
         System.out.println("Lower case string: " + lowerCaseString);
         System.out.println("Edited string: " + replacedCharsString);
         System.out.println("First two chars: " + substring);
 
-        System.out.println("String starts with T: " + startsWith);
+        System.out.println("String starts with " + prefix + ": "+ startsWith);
         System.out.println("String is empty: " + isEmpty);
         System.out.println("Contains 'DUMMY' string: " + containsString);
 
-        System.out.println("Index of 'DUMMY' string: " + indexOf);
+        System.out.println("***Index of string: " + indexOf);
         System.out.println("Compared to different case string: " + match);
         System.out.println("Length of string: " + length);
 
         System.out.println("First char: " + firstChar);
         System.out.println("Last char: " + lastChar);
 
-
-        int amount = 10;
-        double price;
-        double total;
-
-        price = 20.0;
-        total = amount * price;
 
         List<String> names = Arrays.asList("Luke", "Leya", "Chewy");
 
@@ -61,6 +55,5 @@ public class StringDemo {
 
         List<String> emptyList = new ArrayList<String>();
         emptyList.add("Item one");
-
     }
 }
